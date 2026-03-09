eac3622b284fd3d7d89486894d45a3534bb631c9
AlanMZmb <montesdeocaalan41@gmail.com>
2026-03-08 21:08:50 -0600

    triangulo

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ function draw() {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
 
-    ctx.fillStyle = "blue";
-    ctx.fillRect(20,20,120,120);
+    ctx.beginPath();
+    ctx.moveTo(75,50);
+    ctx.lineTo(100,75);
+    ctx.lineTo(100,25);
+    ctx.fill();
 
 }
