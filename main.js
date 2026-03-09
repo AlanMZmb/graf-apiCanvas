8dafda590e18373962559e5778348d9933dacfd7
AlanMZmb <montesdeocaalan41@gmail.com>
2026-03-08 21:02:16 -0600

    Rectangulo

---
+++ main.js
@@ -0,0 +1,12 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+function draw() {
+
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "blue";
+    ctx.fillRect(20,20,120,120);
+
+}
